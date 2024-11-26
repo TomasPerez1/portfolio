59d865064eb395420b81557dcded511b31f230dd
tomasPerez <tomasperez.henry@gmail.com>
2024-11-26 19:41:31 -0300

    typescript config

---
+++ my-app/src/app/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p className="border-2 border-red-500">Dashboard Page</p>;
+}
---
+++ my-app/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { lusitana } from "./ui/fonts";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6 border-2 border-red-700 bg-yellow-300">
+      <h1 className="text-white">Hola que talllll</h1>
+    </main>
+  );
+}
