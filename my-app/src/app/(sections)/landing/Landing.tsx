2f173e1e3b4e9adabb3f5a824b0eeba175e09940
tomasPerez <tomasperez.henry@gmail.com>
2024-11-29 18:07:47 -0300

    initialize

--- my-app/src/app/(sections)/landing/Landing.tsx
+++ my-app/src/app/(sections)/landing/Landing.tsx
@@ -4,10 +4,14 @@ export default function Landing() {
   return (
     <section className="border-2  min-h-screen">
       <article className=" border-2 border-yellow-400 text-center w-fit">
-        <h1 className="text-8xl">Tomas <br /> Perez</h1>
+        <h1 className="text-8xl">
+          Tomas <br /> Perez
+        </h1>
         <h2 className="text-2xl text-[#cda3ff]">FULL STACK DEVELOPER</h2>
       </article>
+      <div className="w-[10rem] h-[10rem] text-center bg-blue-700 border-2 border-blue-700">
+        <h1>Soy el trhee js</h1>
+      </div>
     </section>
   );
 }
-
