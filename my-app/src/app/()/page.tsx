7022ef8316f7361f831049bd038b46a3fa2587af
tomasPerez <tomasperez.henry@gmail.com>
2024-11-29 13:58:32 -0300

    basic schedule

--- my-app/src/app/()/page.tsx
+++ my-app/src/app/()/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
-import { lusitana } from "./ui/fonts";
+
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6 border-2 border-red-700 bg-yellow-300">
-      <h1 className="text-white">Hola que talllll</h1>
+      <h1 className="text-white">Soy el jojoooo</h1>
     </main>
   );
 }
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-6 border-2 border-red-700 bg-yellow-300">
+      <h1 className="text-white">Hola que talllll</h1>
+    </main>
+  );
+}
