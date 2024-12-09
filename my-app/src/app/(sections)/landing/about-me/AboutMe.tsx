6e15e44e4493ee6477f91e933481a4f3b1ddd29e
tomasPerez <tomasperez.henry@gmail.com>
2024-12-09 11:29:01 -0300

    new collage

--- my-app/src/app/(sections)/landing/about-me/AboutMe.tsx
+++ my-app/src/app/(sections)/landing/about-me/AboutMe.tsx
@@ -1,7 +1,49 @@
+"use client";
+import Image from "next/image";
+import { AnimatedTestimonials } from "./AnimatedTestimonials";
+
 export default function AboutMe() {
+  type Testimonial = {
+    quote: string;
+    name: string;
+    designation: string;
+    src: string;
+  };
+  const testimonials: Testimonial[] = [
+    {
+      quote: "Una cita inspiradora",
+      name: "web",
+      designation: "Desarrollador Web",
+      src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/internet2.png",
+    },
+    {
+      quote: "Otra cita inspiradora",
+      name: "wpp",
+      designation: "Gerente de Proyecto",
+      src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/wpp.png",
+    },
+    {
+      quote: "Una tercera cita inspiradora",
+      name: "ig",
+      designation: "Especialista en Redes Sociales",
+      src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/instagram.png",
+    },
+  ];
+
   return (
     <section id="about-me" className="border-2 border-green-500 min-h-screen">
-      <h1>aboutt</h1>
+      <figure className="max-w-[350px]">
+        <Image
+          src={"/profile_box.JPG"}
+          alt="profile image"
+          width={150}
+          height={150}
+          // layout="responsive"
+        />
+      </figure>
+      <hr />
+      <AnimatedTestimonials testimonials={testimonials} />
+      {/* Flyon Carroucel */}
     </section>
   );
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flyonui/dist/js/*.js",
   ],
   theme: {
     extend: {
