cefd68ad84fa1949fded565cd8cd31f8d2df521a
tomasPerez <tomasperez.henry@gmail.com>
2024-12-09 09:09:59 -0300

    clean old 3D model

--- my-app/src/app/(sections)/landing/Landing.tsx
+++ my-app/src/app/(sections)/landing/Landing.tsx
@@ -9,7 +9,7 @@ export default function Landing() {
       id="about-me"
       className="min-h-screen w-full border-2 border-yellow-400 flex flex-col justify-between"
     >
-      <div className="border-2 border-green-500  w-fit mx-auto relative">
+      <div className="border-2 border-green-500  w-fit ml-auto relative">
         <Spline scene="https://prod.spline.design/OoBR4Z3NWKxkJac9/scene.splinecode" />
         <span className="flex gap-2 p-1  items-center justify-center rounded-lg w-[150px] h-[37px] bg-gray-900 text-white absolute z-50 right-5 bottom-[20px]">
           <RiArrowGoForwardFill className="w-4" />
