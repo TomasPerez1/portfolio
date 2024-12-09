72a5547842b1026e792fb4de01342e56731e558d
tomasPerez <tomasperez.henry@gmail.com>
2024-12-09 12:23:50 -0300

    Refactor AboutMe component and remove AnimatedTestimonials
    - Removed the AnimatedTestimonials component and its related functionality.
    - Introduced ProfileCarroucel for displaying images instead of testimonials.
    - Updated package.json and package-lock.json to remove @tabler/icons-react dependency.
    - Simplified the AboutMe component structure for better clarity and performance.

--- my-app/src/app/(sections)/landing/about-me/AboutMe.tsx
+++ my-app/src/app/(sections)/landing/about-me/AboutMe.tsx
@@ -1,49 +1,29 @@
 "use client";
-import Image from "next/image";
-import { AnimatedTestimonials } from "./AnimatedTestimonials";
+import { ProfileCarroucel } from "./ProfileCarroucel";
 
 export default function AboutMe() {
-  type Testimonial = {
-    quote: string;
+  type Imgs = {
     name: string;
-    designation: string;
     src: string;
   };
-  const testimonials: Testimonial[] = [
+  const imgs: Imgs[] = [
     {
-      quote: "Una cita inspiradora",
       name: "web",
-      designation: "Desarrollador Web",
       src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/internet2.png",
     },
     {
-      quote: "Otra cita inspiradora",
       name: "wpp",
-      designation: "Gerente de Proyecto",
       src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/wpp.png",
     },
     {
-      quote: "Una tercera cita inspiradora",
       name: "ig",
-      designation: "Especialista en Redes Sociales",
       src: "https://res.cloudinary.com/dnxa8khx9/image/upload/v1730993636/scraper/instagram.png",
     },
   ];
 
   return (
     <section id="about-me" className="border-2 border-green-500 min-h-screen">
-      <figure className="max-w-[350px]">
-        <Image
-          src={"/profile_box.JPG"}
-          alt="profile image"
-          width={150}
-          height={150}
-          // layout="responsive"
-        />
-      </figure>
-      <hr />
-      <AnimatedTestimonials testimonials={testimonials} />
-      {/* Flyon Carroucel */}
+      <ProfileCarroucel imgs={imgs} autoplay={true} />
     </section>
   );
 }
---
+++ my-app/src/app/(sections)/landing/about-me/ProfileCarroucel.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { RiArrowLeftLine, RiArrowRightLine } from "@remixicon/react";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+type Imgs = {
+  name: string;
+  src: string;
+};
+
+export const ProfileCarroucel = ({
+  imgs,
+  autoplay = false,
+}: {
+  imgs: Imgs[];
+  autoplay?: boolean;
+}) => {
+  const [active, setActive] = useState(0);
+
+  const handleNext = () => {
+    setActive((prev) => (prev + 1) % imgs.length);
+  };
+
+  const handlePrev = () => {
+    setActive((prev) => (prev - 1 + imgs.length) % imgs.length);
+  };
+
+  const isActive = (index: number) => {
+    return index === active;
+  };
+
+  useEffect(() => {
+    if (autoplay) {
+      const interval = setInterval(handleNext, 5000);
+      return () => clearInterval(interval);
+    }
+  }, [autoplay]);
+
+  const randomRotateY = () => {
+    return Math.floor(Math.random() * 21) - 10;
+  };
+  return (
+    <div className=" mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
+      <div className="flex flex-col gap-10 w-fit">
+        <picture className="">
+          <div className="relative h-[20rem] w-[20rem]">
+            <AnimatePresence>
+              {imgs.map((img, index) => (
+                <motion.div
+                  key={img.src}
+                  initial={{
+                    opacity: 0,
+                    scale: 0.9,
+                    z: -100,
+                    rotate: randomRotateY(),
+                  }}
+                  animate={{
+                    opacity: isActive(index) ? 1 : 0.7,
+                    scale: isActive(index) ? 1 : 0.95,
+                    z: isActive(index) ? 0 : -100,
+                    rotate: isActive(index) ? 0 : randomRotateY(),
+                    zIndex: isActive(index) ? 999 : imgs.length + 2 - index,
+                    y: isActive(index) ? [0, -80, 0] : 0,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    scale: 0.9,
+                    z: 100,
+                    rotate: randomRotateY(),
+                  }}
+                  transition={{
+                    duration: 0.4,
+                    ease: "easeInOut",
+                  }}
+                  className="absolute inset-0 origin-bottom"
+                >
+                  <Image
+                    src={img.src}
+                    alt={img.name}
+                    width={500}
+                    height={500}
+                    draggable={false}
+                    className="h-full w-full rounded-3xl object-cover object-center"
+                  />
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          </div>
+        </picture>
+
+        <section
+          id="buttons"
+          className="flex justify-start gap-4 w-fit mx-auto "
+        >
+          <button
+            onClick={handlePrev}
+            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button hover:bg-neutral-800 hover:text-white transition-colors"
+          >
+            <RiArrowLeftLine className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:text-white" />
+          </button>
+          <button
+            onClick={handleNext}
+            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button hover:bg-neutral-800 hover:text-white transition-colors"
+          >
+            <RiArrowRightLine className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:text-white" />
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+};
