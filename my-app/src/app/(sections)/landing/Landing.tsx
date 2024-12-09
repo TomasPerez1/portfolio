5e52c4466746eb272d33ce884e9b7e3155651dcf
tomasPerez <tomasperez.henry@gmail.com>
2024-12-09 00:22:15 -0300

    delete three

--- my-app/src/app/(sections)/landing/Landing.tsx
+++ my-app/src/app/(sections)/landing/Landing.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 const Scene = dynamic(() => import("../../ui/Scene"), { ssr: false });
+import AbstractCube from "../../ui/ParticleEffect";
 
 export default function Landing() {
   return (
@@ -9,6 +10,9 @@ export default function Landing() {
       id="about-me"
       className="min-h-screen w-full border-2 border-yellow-400 flex flex-col justify-between"
     >
+      <div className="border-2 border-green-600 w-[750px] h-[750px]">
+        <AbstractCube />
+      </div>
       {/* <article className="text-center">
         <span className="border-2">
           <h1 className="text-8xl">
@@ -18,12 +22,9 @@ export default function Landing() {
         </span>
       </article> */}
 
-      <div className="flex gap-10">
-        <div className="w-fit border-2 border-red-500">
-          <Scene />
-        </div>
-        <div className="border-2 border-red-500 w-[420px] h-[420px]"></div>
-      </div>
+      {/* <div className=" border-2 border-red-500 ">
+        <Scene />
+      </div> */}
 
       {/* <div className="w-fit mx-auto mb-8 animate-bounce ">
         <p className="text-5xl ">👇</p>
--- my-app/src/app/ui/Scene.tsx
+++ my-app/src/app/ui/Scene.tsx
@@ -3,28 +3,18 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import { Suspense } from "react";
 import { useProgress, Html, ScrollControls } from "@react-three/drei";
-import CubeModel from "./CubeModel";
-
-function Loader() {
-  // const { progress, active } = useProgress();
-
-  // return <Html center>{progress.toFixed(1)} % loaded</Html>;
-  return (
-    <div className="bg-orange-500 text-white">
-      <h1>Estoy cargadno</h1>
-    </div>
-  );
-}
+import AbstractCube from "./ParticleEffect";
+// import CubeModel from "./ParticleEffect";
 
 export default function Scene() {
   return (
-    <Canvas gl={{ antialias: true }} dpr={[1, 1.15]}>
-      <directionalLight position={[5, 5, 5]} intensity={4} />
-      <Suspense fallback={<Loader />}>
-        <ScrollControls>
-          <CubeModel />
-        </ScrollControls>
-      </Suspense>
-    </Canvas>
+    <AbstractCube />
+
+    // <Canvas gl={{ antialias: true }} dpr={[1, 1.15]}>
+    //   <directionalLight position={[1, 1, 1]} intensity={10} />
+    //   <Suspense fallback={null}>
+    //     <CubeModel />
+    //   </Suspense>
+    // </Canvas>
   );
 }
