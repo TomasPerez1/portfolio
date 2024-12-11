6ac8ab32164be4ca42603f8b60d22c2d4f1a9d43
tomasPerez <tomasperez.henry@gmail.com>
2024-12-11 16:45:58 -0300

    add calendly iframe

--- my-app/src/app/(sections)/landing/contact/Contact.tsx
+++ my-app/src/app/(sections)/landing/contact/Contact.tsx
@@ -3,9 +3,24 @@ import { InlineWidget } from "react-calendly";
 export default function Contact() {
   return (
     <section id="contact" className="min-h-screen pb-[6rem]">
-      <div className="w-[90%] mx-auto gap-8 grid grid-cols-2 ">
-        <h1>SOY EL CONTACTT</h1>
-        <InlineWidget url="https://calendly.com/tomas-perez-developer/30min" />
+      <div className="w-[90%]  mx-auto gap-8 grid grid-cols-2 text-lg">
+        <h1 className="rounded-lg">SOY EL CONTACTT</h1>
+        <InlineWidget
+          styles={{
+            height: "610px",
+            border: "5px solid green",
+            borderRadius: "10px",
+            scrollbarWidth: "thin",
+            scrollbarColor: "#4d4d4d #2c2c2c",
+          }}
+          pageSettings={{
+            backgroundColor: "#ffffff",
+            hideGdprBanner: true,
+            hideEventTypeDetails: true,
+            hideLandingPageDetails: true,
+          }}
+          url="https://calendly.com/tomas-perez-developer/30min"
+        />
       </div>
     </section>
   );
