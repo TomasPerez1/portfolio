a934738c4a71926ec85cf098e7a36de830d4b8ff
tomasPerez <tomasperez.henry@gmail.com>
2024-12-11 16:54:17 -0300

    Update Contact component styles and Calendly widget settings

--- my-app/src/app/(sections)/landing/contact/Contact.tsx
+++ my-app/src/app/(sections)/landing/contact/Contact.tsx
@@ -3,20 +3,20 @@ import { InlineWidget } from "react-calendly";
 export default function Contact() {
   return (
     <section id="contact" className="min-h-screen pb-[6rem]">
-      <div className="w-[90%]  mx-auto gap-8 grid grid-cols-2 text-lg">
-        <h1 className="rounded-lg">SOY EL CONTACTT</h1>
+      <div className="w-[90%]  mx-auto gap-8 grid grid-cols-2 text-lg ">
+        <h1 className="rounded-lg bg-gray-800">SOY EL CONTACTT</h1>
         <InlineWidget
           styles={{
             height: "610px",
-            border: "5px solid green",
+            border: "5px solid purple",
             borderRadius: "10px",
             scrollbarWidth: "thin",
             scrollbarColor: "#4d4d4d #2c2c2c",
           }}
           pageSettings={{
-            backgroundColor: "#6A669D",
-            primaryColor: "#ffffff",
-            textColor: "#640D5F",
+            backgroundColor: "#1f2937",
+            primaryColor: "#640D5F",
+            textColor: "#ffffff",
             hideGdprBanner: true,
             hideEventTypeDetails: true,
             hideLandingPageDetails: true,
