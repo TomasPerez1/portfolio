b35ffa4185f2aaec414bf6ab4fc477dd3d7b6daa
tomasPerez <tomasperez.henry@gmail.com>
2024-12-10 22:19:55 -0300

    add svgs

--- my-app/src/app/(sections)/landing/proyects/ProyectCard.tsx
+++ my-app/src/app/(sections)/landing/proyects/ProyectCard.tsx
@@ -9,11 +9,11 @@ import { Carousel } from "./Carousel";
 import Image from "next/image";
 
 export default function ProyectCard({ proyect }) {
-  const { id, name, description, github, imgs } = proyect;
+  const { id, name, description, github, imgs, skills } = proyect;
 
   return (
-    <Card className="py-2 bg-gray-600 flex-shrink-0 max-h-fit ">
-      <CardHeader className=" px-0 flex-col items-start">
+    <Card className="py-0 bg-gray-600 flex-shrink-0 max-h-fit relative">
+      <CardHeader className="px-0 flex-col items-start">
         <Carousel imgs={imgs} />
       </CardHeader>
       <CardBody className="overflow-visible py-0">
@@ -31,6 +31,29 @@ export default function ProyectCard({ proyect }) {
           </AccordionItem>
         </Accordion>
       </CardBody>
+      <Skills logos={skills} />
     </Card>
+  );
+}
+
+function Skills({ logos }) {
+  return (
+    <div className="w-full flex gap-2.5 flex-wrap absolute z-50 bg-white/35 rounded-sm backdrop-blur-sm px-1.5 py-1">
+      {logos?.length ? (
+        logos?.map((logo, index) => (
+          <div key={index} className="w-10 h-10">
+            <Image
+              src={logo.src}
+              alt="skill_logo"
+              width={40}
+              height={40}
+              className="w-full h-full object-contain"
+            />
+          </div>
+        ))
+      ) : (
+        <h1 className="text-white bg-red-500">NO TIENE</h1>
+      )}
+    </div>
   );
 }
--- my-app/src/app/(sections)/landing/proyects/Proyects.tsx
+++ my-app/src/app/(sections)/landing/proyects/Proyects.tsx
@@ -1,12 +1,24 @@
 import ProyectCard from "./ProyectCard";
+import cloudinary from "@public/skills/cloudinary-icon.svg";
+import express from "@public/skills/express.svg";
+import framer from "@public/skills/framer.svg";
+import nestjs from "@public/skills/nestjs.svg";
+import nextjs from "@public/skills/nextjs-icon.svg";
+import prisma from "@public/skills/prisma.svg";
+import react from "@public/skills/react.svg";
+import tailwind from "@public/skills/tailwindcss-icon.svg";
+import typescript from "@public/skills/typescript-icon.svg";
+import sequelize from "@public/skills/sequelize.svg";
 
 export default function Proyects() {
+  console.log(cloudinary, "svgs");
   type Proyect = {
     id: number;
     name: string;
     description: string;
     github: string;
     imgs: string[];
+    skills: string[];
   };
   const proyects: Proyect[] = [
     {
@@ -17,10 +29,11 @@ export default function Proyects() {
       github: "https://github.com/ramirofazio/my-fotolibro",
       imgs: [
         "https://nextui.org/images/hero-card-complete.jpeg",
-        "https://nextui.org/images/card-example-2.jpeg",
-        "https://nextui.org/images/card-example-6.jpeg",
+        "https://nextui.org/images/hero-card-complete.jpeg",
+        "https://nextui.org/images/hero-card-complete.jpeg",
         "https://nextui.org/images/hero-card-complete.jpeg",
       ],
+      skills: [react, tailwind, express, cloudinary],
     },
     {
       id: 2,
@@ -33,6 +46,7 @@ export default function Proyects() {
         "https://nextui.org/images/hero-card-complete.jpeg",
         "https://nextui.org/images/hero-card-complete.jpeg",
       ],
+      skills: [cloudinary, express, react],
     },
     {
       id: 3,
@@ -45,6 +59,7 @@ export default function Proyects() {
         "https://nextui.org/images/hero-card-complete.jpeg",
         "https://nextui.org/images/hero-card-complete.jpeg",
       ],
+      skills: [cloudinary, express, react],
     },
     {
       id: 4,
@@ -57,12 +72,13 @@ export default function Proyects() {
         "https://nextui.org/images/hero-card-complete.jpeg",
         "https://nextui.org/images/hero-card-complete.jpeg",
       ],
+      skills: [cloudinary, express, react],
     },
   ];
   return (
     <section id="proyects" className="min-h-screen  ">
       <div className="w-[90%] mx-auto gap-8 grid grid-cols-2 ">
-        {proyects.reverse().map((proyect, i) => (
+        {proyects.map((proyect, i) => (
           <ProyectCard key={proyect.id} proyect={proyect} />
         ))}
       </div>
