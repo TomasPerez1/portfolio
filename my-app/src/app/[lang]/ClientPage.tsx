63dbee3009d3907db88f824a39416ea345caad99
Pelucheado <tomas.perez.developer@gmail.com>
2026-05-10 21:59:33 -0300

    feat(phase-6): wire all redesign sections into ClientPage (v1 sections removed)

--- my-app/src/app/[lang]/ClientPage.tsx
+++ my-app/src/app/[lang]/ClientPage.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import AboutMe from "../(sections)/landing/about-me/AboutMe";
-import Proyects from "../(sections)/landing/proyects/Proyects";
-import Contact from "../(sections)/landing/contact/Contact";
 import LangLoader from "../ui/LangLoader";
 import { useTranslation } from "../i18n/client";
 import NavWrapper from "../components/redesign/wrappers/NavWrapper";
 import HeroWrapper from "../components/redesign/wrappers/HeroWrapper";
+import FeaturedWorkWrapper from "../components/redesign/wrappers/FeaturedWorkWrapper";
+import ProjectsGridWrapper from "../components/redesign/wrappers/ProjectsGridWrapper";
+import StackSectionWrapper from "../components/redesign/wrappers/StackSectionWrapper";
+import ExperienceWrapper from "../components/redesign/wrappers/ExperienceWrapper";
+import AboutWrapper from "../components/redesign/wrappers/AboutWrapper";
+import ContactWrapper from "../components/redesign/wrappers/ContactWrapper";
+import FooterWrapper from "../components/redesign/wrappers/FooterWrapper";
+import { StackTicker } from "../components/redesign/FeaturedWork";
 
 interface ClientPageProps {
   lang?: string;
@@ -14,18 +19,20 @@ interface ClientPageProps {
 
 function ClientPage({ lang = "en" }: ClientPageProps) {
   const { ready } = useTranslation(lang, "common");
-  return ready ? (
+  if (!ready) return <LangLoader />;
+  return (
     <main className="bg-bg min-h-screen flex flex-col">
       <NavWrapper lang={lang} />
       <HeroWrapper lang={lang} />
-      <section className="flex flex-col gap-8 bg-grid-small-white/[0.2]" id="content">
-        <AboutMe lang={lang} />
-        <Proyects lang={lang} />
-        <Contact lang={lang} />
-      </section>
+      <StackTicker />
+      <FeaturedWorkWrapper lang={lang} />
+      <ProjectsGridWrapper lang={lang} />
+      <StackSectionWrapper lang={lang} />
+      <ExperienceWrapper lang={lang} />
+      <AboutWrapper lang={lang} />
+      <ContactWrapper lang={lang} />
+      <FooterWrapper lang={lang} />
     </main>
-  ) : (
-    <LangLoader />
   );
 }
 
